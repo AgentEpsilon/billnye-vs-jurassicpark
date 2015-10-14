6e401af0e5789a6e54f1dd47bd0bae43f9e63920
Evan Miller <miller.evan815@gmail.com>
2015-10-14 19:43:44 -0400

    Started PlayerDataManager

---
+++ Assets/PlayerDataManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDataManager : MonoBehaviour {
+
+	public int health { get; private set; }
+	public int maxHealth { get; private set; }
+
+	public void Damage(int amt){
+		health = Mathf.Max (health - amt, 0);
+	}
+
+	public void Heal(int amt){
+		health = Mathf.Min (health + amt, maxHealth);
+	}
+
+	void Awake(){
+		DontDestroyOnLoad (gameObject);
+		maxHealth = PlayerPrefs.GetInt("maxHealth", 20);
+		health = PlayerPrefs.GetInt ("health", maxHealth);
+	}
+}
