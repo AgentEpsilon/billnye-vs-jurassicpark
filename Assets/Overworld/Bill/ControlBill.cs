be8cab68a0a5828419e614216793413d2f189e67
Evan Miller <miller.evan815@gmail.com>
2015-10-14 19:43:23 -0400

    Rearranged folders & renamed rigidbody

--- Assets/Overworld/Bill/ControlBill.cs
+++ Assets/Overworld/Bill/ControlBill.cs
@@ -5,20 +5,20 @@ public class ControlBill : MonoBehaviour {
 
 	public float speed;
 	private Animator anim;
-	private Rigidbody2D rigidbody;
+	private Rigidbody2D rigidbody2D;
 	
 	[SerializeField] private float horiz;
 	[SerializeField] private float verti;
 
 	void Start(){
 		anim = GetComponent<Animator> ();
-		rigidbody = GetComponent<Rigidbody2D> ();
+		rigidbody2D = GetComponent<Rigidbody2D> ();
 	}
 	
 	void Update () {
 		horiz = Input.GetAxis ("Horizontal");
 		verti = Input.GetAxis ("Vertical");
-		rigidbody.velocity = new Vector2(Mathf.Lerp(0, horiz* speed, 0.8f),
+		rigidbody2D.velocity = new Vector2(Mathf.Lerp(0, horiz* speed, 0.8f),
 		                                   Mathf.Lerp(0, verti* speed, 0.8f));
 		if (horiz != 0f || verti != 0f)
 			anim.SetBool ("isWalking", true);
