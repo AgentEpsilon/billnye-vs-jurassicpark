daf420c0f1873cea677a82f07433f7dd89869cb2
Evan Miller <miller.evan815@gmail.com>
2015-10-29 18:53:02 -0400

    Woodchopping encounter playable

---
+++ Assets/Encounters/TreeMinigamePC.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TreeMinigamePC : MonoBehaviour {
+
+	private bool right = false;
+	private bool finish = false;
+	private readonly int displacement = 1;
+	public float completed {get; private set;}
+	[SerializeField] public Slider progress;
+	[SerializeField] public GameObject victory;
+	
+	// Update is called once per frame
+	void Update () {
+		if(!finish){
+			if(Input.GetAxis("Horizontal")>0&&!right){
+				transform.Translate(transform.right*displacement);
+				right = !right;
+				completed = Mathf.Min (completed+5f, 100f);
+			}else if(Input.GetAxis("Horizontal")<0&&right){
+				transform.Translate(-transform.right*displacement);
+				right = !right;
+				completed = Mathf.Min (completed+5f, 100f);
+			}
+			completed = Mathf.Max(completed-Time.deltaTime*20f, 0f);
+			progress.value = completed;
+			if(completed>95f){
+				finish = true;
+				victory.SetActive(true);
+			}
+		}
+	}
+}
